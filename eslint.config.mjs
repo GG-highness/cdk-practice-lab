a3a59b90176b6aec5b46d8fa6119187fdc164637
dice0155 <f.dice.1997@gmail.com>
2024-10-22 20:32:52 +0900

    edit: lint function

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import globals from "globals";
 import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
 
 export default [
   {
@@ -8,11 +10,29 @@ export default [
     languageOptions: {
       parser: typescriptParser,
       globals: {
-        ...globals.browser
+        ...globals.browser,
       },
     },
-    plugins: { "@typescript-eslint": typescriptPlugin },
-    rules: { ...typescriptPlugin.configs.recommended.rules },
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+      "import": importPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      "import/order": [
+        "error",
+        {
+          "groups": [["builtin", "external", "internal"]],
+          "alphabetize": { "order": "asc", "caseInsensitive": true },
+        }
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" },
+      ],
+    },
   },
   {
     files: ["**/*.js"],
