7d0e01fa361928aa7d2485fbf09ba170fa1cd39f
dice0155 <f.dice.1997@gmail.com>
2024-10-22 20:35:51 +0900

    edit: lint function

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 import importPlugin from "eslint-plugin-import";
 import unusedImports from "eslint-plugin-unused-imports";
+import vitestPlugin from "eslint-plugin-vitest";
 import globals from "globals";
 
 export default [
@@ -17,6 +18,7 @@ export default [
       "@typescript-eslint": typescriptPlugin,
       "import": importPlugin,
       "unused-imports": unusedImports,
+      "vitest": vitestPlugin,
     },
     rules: {
       ...typescriptPlugin.configs.recommended.rules,
@@ -25,13 +27,16 @@ export default [
         {
           "groups": [["builtin", "external", "internal"]],
           "alphabetize": { "order": "asc", "caseInsensitive": true },
-        }
+        },
       ],
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
         { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" },
       ],
+      "vitest/no-focused-tests": "error",
+      "vitest/no-identical-title": "error",
+      "vitest/valid-expect": "error",
     },
   },
   {
